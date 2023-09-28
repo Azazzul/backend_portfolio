bf55415c35515fb8b61aa8ebd78e1e90670dae54
BENOIT FARDOUX <benoit.fardoux@worldline.com>
2023-09-28 14:36:31 +0200

    update size

--- index.js
+++ index.js
@@ -113,84 +113,9 @@ app.get("/api/skill/:id", (req, res, next) => {
 });
 
 
-app.post("/api/skill/", (req,res,next) => {
-    const errors = [];
-    console.log(req.body)
-    if (!req.body.name){
-        errors.push('No name specified')
-    }
-    if (!req.body.image){
-        errors.push('No image specified')
-    }
-
-    if (!req.body.description){
-        errors.push('No description specified')
-    }
-    if (errors.length){
-        res.json({"error" : errors.join(",")})
-        return
-    }
-    const data = {
-        name : req.body.name,
-        image : req.body.image,
-        description : req.body.description
-    }
-    const  sql = 'INSERT INTO SKILL (name, image, description) VALUES (?,?,?)'
-    const params = [data.name,data.image,data.description]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-app.delete("/api/skill/:id", (req,res,next) => {
-    db.run(
-        'DELETE FROM SKILL WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", changes: this.changes})
-    });
-})
-
-app.patch("/api/skill/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        image: req.body.image,
-        description : req.body.description
-    }
-    db.run(
-        `UPDATE SKILL set
-           name = COALESCE(?,name),
-           image = COALESCE(?,image),
-           description = COALESCE(?,description)
-           WHERE id = ?`,
-        [data.name, data.image, data.description, req.params.id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": err.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data,
-                changes: this.changes
-            })
-    });
-})
 
 app.get("/api/projects", (req,res,next) => {
-    const sql = "select * from PROJECT ORDER BY dates DESC"
+    const sql = "select * from PROJECT"
     const params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -219,114 +144,9 @@ app.get("/api/projects/:id", (req, res, next) => {
     });
 });
 
-app.post("/api/projects/", (req,res,next) => {
-    const errors = [];
-    console.log(req.body)
-    if (!req.body.title){
-        errors.push('No name specified')
-    }
-    if (!req.body.image){
-        errors.push('No image specified')
-    }
-
-    if (!req.body.description){
-        errors.push('No description specified')
-    }
-    if (!req.body.link){
-        errors.push('No link specified')
-    }
-
-    if (!req.body.dates){
-        errors.push('No date specified')
-    }
-
-    if (!req.body.client){
-        errors.push('No client specified')
-    }
-
-
-    if (errors.length){
-        res.json({"error" : errors.join(",")})
-        return
-    }
-    const data = {
-        title : req.body.title,
-        image : req.body.image,
-        description : req.body.description,
-        lien : req.body.link,
-        dates : req.body.dates,
-        client : req.body.client
-    }
-    const  sql = 'INSERT INTO PROJECT (title, image, description, lien, dates, client) VALUES (?,?,?,?,?,?)'
-    const params = [data.title,data.image,data.description, data.lien, data.dates, data.client]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-
-app.patch("/api/projects/:id", (req, res, next) => {
-    const data = {
-        title : req.body.title,
-        image : req.body.image,
-        description : req.body.description,
-        lien : req.body.link,
-        dates : req.body.dates,
-        client : req.body.client
-    }
-    db.run(
-        `UPDATE PROJECT set
-           title = COALESCE(?,title),
-           image = COALESCE(?,image),
-           description = COALESCE(?,description),
-           lien = COALESCE(?,lien) ,
-           dates = COALESCE(?,dates),
-           client = COALESCE(?,client)
-           WHERE id = ?`,
-        [data.title,data.image,data.description, data.lien, data.dates,data.client, req.params.id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": err.message})
-                console.log(err)
-                console.log(req)
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data,
-                changes: this.changes
-            })
-    });
-})
-
-
-
-
-
-app.delete("/api/projects/:id", (req,res,next) => {
-    db.run(
-        'DELETE FROM PROJECT WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", changes: this.changes})
-    });
-})
-
 
 app.get('/api/jobs/',(req,res,next) => {
-    const SQL_REQUEST = 'SELECT * FROM JOBS ORDER BY date_fin';
+    const SQL_REQUEST = 'SELECT * FROM JOBS';
     const params = []
     db.all(SQL_REQUEST, params, (err, rows) => {
         if (err) {
@@ -340,77 +160,7 @@ app.get('/api/jobs/',(req,res,next) => {
     })
 });
 
-app.post("/api/jobs/", (req,res,next) => {
-    const errors = [];
-    console.log(req.body)
-    if (!req.body.title){
-        errors.push('No name specified')
-    }
-    if (!req.body.image){
-        errors.push('No image specified')
-    }
 
-    if (!req.body.job_description){
-        errors.push('No description specified')
-    }
-    if (!req.body.skill){
-        errors.push('No skill specified')
-    }
-
-    if (!req.body.duration){
-        errors.push('No duration specified')
-    }
-
-    if (!req.body.date_debut){
-        errors.push('No date_debut specified')
-    }
-
-    if (!req.body.date_fin){
-        errors.push('No date_fin specified')
-    }
-    if (!req.body.business){
-        errors.push('No client specified')
-    }
-
-
-    if (errors.length){
-        res.json({"error" : errors.join(",")})
-        return
-    }
-    const data = {
-        title : req.body.title,
-        image : req.body.image,
-        skill: req.body.skill,
-        duration: req.body.durations,
-        description : req.body.job_description,
-        lien : req.body.link,
-        date_debut : req.body.date_debut,
-        date_fin : req.body.date_fin,
-        business: req.body.business
-    }
-    const  sql = 'INSERT INTO JOBS (title, image, job_description, competences,durations, date_debut,date_fin, business_name) VALUES (?,?,?,?,?,?,?,?)'
-    const params = [
-        data.title,
-        data.image,
-        data.description,
-        data.skill,
-        data.duration,
-        data.date_debut,
-        data.date_fin,
-        data.business
-        ]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
 
 // Default response for any other request
 app.get('*' , function(req, res){
